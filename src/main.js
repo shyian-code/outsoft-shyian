d3fb485a2d9874c32f0a551fee13bd0820d79685
shyian-code <shyian.code@gmail.com>
2024-11-09 21:54:45 +0200

    accordion functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+let acc = document.getElementsByClassName("faq-section__accordion-title");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
