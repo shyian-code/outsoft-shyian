f0096f063eb179db60e8098c784d2f81f4656b17
shyian-code <shyian.code@gmail.com>
2024-11-10 00:22:18 +0200

    add burger menu icon

--- src/main.js
+++ src/main.js
@@ -12,3 +12,8 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+function toggleMenu() {
+  const menu = document.querySelector('.main-header__wrapper');
+  menu.classList.toggle('open');
+}
